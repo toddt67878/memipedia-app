058596b1f01de3ba5a3ae52be225b469ab517d2d
Todd Traylor <toddt67878@yahoo.com>
2020-04-06 11:37:01 -0500

    Installing React Naviagation Libraries

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import FeedScreen from "./screens/FeedScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FeedScreen />
     </View>
   );
 }
@@ -12,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "aqua",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
